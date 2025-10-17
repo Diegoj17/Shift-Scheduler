49146f0b3fd4dacf5bbb340a8ea92cbe1ba5eaca
Diego_jt <diegotorrado16@gmail.com>
2025-10-17 13:19:26 -0500

    Cambio

--- src/components/management/TableControls.jsx
+++ src/components/management/TableControls.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaSearch } from "react-icons/fa";
-import userService from '../../services/userService';
+import { userService } from '../../services/userService';
 import '../../styles/components/management/TableControls.css';
 
 const TableControls = ({
