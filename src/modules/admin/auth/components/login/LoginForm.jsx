818abc184f5398f3ec1c37775d48b47ab18077bb
Diego_jt <99993658+Diegoj17@users.noreply.github.com>
2025-10-21 15:03:56 -0500

    Fix import path for AuthContext in LoginForm

--- src/modules/admin/auth/components/login/LoginForm.jsx
+++ src/modules/admin/auth/components/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '/src/contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { FaUser } from "react-icons/fa";
 import { Link } from 'react-router-dom';
